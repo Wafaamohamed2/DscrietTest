1db4a37d06f292ab338037711b72152f546fe815
Wafaamohamed2 <121331529+Wafaamohamed2@users.noreply.github.com>
2022-12-24 09:08:33 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int low =int.Parse(Console.ReadLine());
+               int high=int.Parse(Console.ReadLine());
+            
+
+                Console.WriteLine("print prime numper from x to y");
+
+            while(low <= high)
+            {
+                Boolean notprime=false;
+
+                for (int i = 2; i <= low / 2; i++)
+                {
+
+                    if (low % i == 0)
+                    {
+                        notprime = true;
+                        break;
+                    }
+                }
+                if (!notprime && low != 0 && low != 0)
+                    Console.WriteLine(low + "");
+
+                ++low;
+                
+            }
+
+        }
+    }
+}
